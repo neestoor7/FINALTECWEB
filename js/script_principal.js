3fe845070cf9918be4ada315c58953d69066e2b9
Miquel Navalón <mnavmoi@upv.edu.es>
2024-01-15 16:13:53 +0100

    Cositas

--- js/script_principal.js
+++ js/script_principal.js
@@ -23,3 +23,15 @@ function adiospubli () {
     document.getElementById('publicidad').style.display = 'none'
 }
 xpubli.onclick = adiospubli
+
+var opinionesObjeto = {
+    comentarios: ["Estoy muy satisfecho con los servicios de Ecolux Energy. ¡Energía limpia y atención al cliente excepcional!","La transición a la energía renovable fue fácil con Ecolux Energy. Recomiendo sus servicios a todos.","Excelente empresa. Su compromiso con la sostenibilidad es impresionante. ¡Gracias por hacer la diferencia!"],
+    usuarios: ["- Núria Martínez","- Mario Pérez","- Sergio Hernández"]
+};
+
+document.getElementById("opinion1").innerHTML = opinionesObjeto.comentarios[0]
+document.getElementById("opinion2").innerHTML = opinionesObjeto.comentarios[1]
+document.getElementById("opinion3").innerHTML = opinionesObjeto.comentarios[2]
+document.getElementById("opinion1.2").innerHTML = opinionesObjeto.usuarios[0]
+document.getElementById("opinion2.2").innerHTML = opinionesObjeto.usuarios[1]
+document.getElementById("opinion3.2").innerHTML = opinionesObjeto.usuarios[2]
