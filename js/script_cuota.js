e077db57e9173db2416b55a0910d56fcec1f23b7
neestoor7 <nespsan@teleco.upv.es>
2024-01-15 17:28:58 +0100

    arreglillos

--- js/script_cuota.js
+++ js/script_cuota.js
@@ -3,23 +3,32 @@ function iniciarArrastre(event) {
 }
 
 function permitirSoltar(event) {
-    event.preventDefault();
+    event.preventDefault(); // Permite que los elementos se puedan soltar en los huecos
 }
 
 function soltar(event) {
-    var idImagen = event.dataTransfer.getData("text");
-    var elementoDestino = document.getElementById(event.target.id);
-    switch (elementoDestino.id) {
-        case "hueco1":
-            document.getElementById(idImagen).src = "imagenes/bombi1.png";
-            break;
-        case "hueco2":
-            document.getElementById(idImagen).src = "imagenes/bombi2.png";
-            break;
-        case "hueco3":
-            document.getElementById(idImagen).src = "imagenes/bombi3.png";
-            break;
-        default:
-            break;
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var bombilla = document.getElementById(data);
+
+    // Asegúrate de que la acción de soltar ocurra en un hueco y no en una imagen u otro elemento
+    var hueco = event.target;
+    if (hueco.className === "hueco" || hueco.className === "imgempresas") {
+        // Si se suelta sobre una imagen dentro del hueco, selecciona el contenedor hueco
+        if (hueco.className === "imgempresas") {
+            hueco = hueco.parentNode;
+        }
+
+        switch(hueco.id) {
+            case "hueco1":
+                bombilla.src = "imagenes/bombi1.png"; // Se queda igual
+                break;
+            case "hueco2":
+                bombilla.src = "imagenes/bombi2.png"; // Cambia a bombi2.png
+                break;
+            case "hueco3":
+                bombilla.src = "imagenes/bombi3.png"; // Cambia a bombi3.png
+                break;
+        }
     }
 }
