99050d82c65bcd8036b2397925268e5fc84d0f41
Miquel Navalón <mnavmoi@upv.edu.es>
2024-01-18 16:02:53 +0100

    VERSIÓN FINAL PORFIN LET'S GO HEMOS TERMINADO OLE OLE OLE

--- js/script_form.js
+++ js/script_form.js
@@ -3,7 +3,6 @@ function calculoCuota() {
     var consumo = document.getElementById('consumoCalculo').value;
     var precioPorKWh;
 
-    // Cálculo de la cuota según el tipo de servicio
     if (tipoServicio === 'residencial') {
         precioPorKWh = 0.12;
     } else if (tipoServicio === 'comercial') {
@@ -11,11 +10,10 @@ function calculoCuota() {
     } else if (tipoServicio === 'industrial') {
         precioPorKWh = 0.20;
     } else {
-        precioPorKWh = 0.15; // Precio por defecto
+        precioPorKWh = 0.15;
     }
 
     var cuota = consumo * precioPorKWh;
 
-    // Mostrar el resultado
     document.getElementById('resultadoCuota').innerText = 'Tu cuota estimada es: ' + cuota.toFixed(2) + ' Euros';
 }
